45423698b6201ef7f91748dca7c1533f212f26ed
Cristian Chitiva <cychitivav@unal.edu.co>
2023-12-30 19:49:19 -0500

    Add solution for Project Euler Problem 1: Multiples of 3 and 5

---
+++ project_euler/001_Multiples_of_3_and_5.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Project Euler, Problem 1: Multiples of 3 and 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
+3, 5, 6, and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Author: Cristian Chitiva <cychitivav@unal.edu.co>
+Date: 30 December 2023
+"""
+
+
+def sum_multiples(n):
+    """
+    This function returns the sum of all the multiples of 3 or 5 below n
+    """
+    total = 0
+    for i in range(n):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+
+    return total
+
+
+if __name__ == "__main__":
+    print(sum_multiples(1000))
